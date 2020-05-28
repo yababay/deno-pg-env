21435a4681557dfad1426a898a7f16b412cf06eb
mabel <js@yababay.ru>
2020-05-28 12:57:05 +0300

    seed.js

--- seed.js
+++ seed.js
@@ -1,16 +1,3 @@
-# Deno's quick PostgreSQL connection
-
-The program uses [denodb](https://deno.land/x/denodb) and [denoenv](https://deno.land/x/denoenv) modules. Please create in current direcrory `.env`-file, for example:
-
-```
-PG_DATABASE = my_db
-PG_USERNAME = me
-PG_PASSWORD = qwerty123
-```
-
-Then use the module as:
-
-```javascript
 import db from 'https://raw.githubusercontent.com/yababay/deno-pg-env/master/mod.js'
 import {DATA_TYPES, Model} from 'https://deno.land/x/denodb/mod.ts';
 
@@ -28,10 +15,3 @@ class Manufacturers extends Model {
 
 db.link([Manufacturers])
 await db.sync({ drop: true });
-```
-
-Run the program with `deno`:
-
-```bash
-deno run --allow-read --allow-env  --allow-net seed.js
-```
