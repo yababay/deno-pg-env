ea03ee8d3c7f3d6b43ceb0c48ef9c0c39d4e1a40
mabel <js@yababay.ru>
2020-05-29 23:11:08 +0300

    Dotenv in separate module

--- mod.js
+++ mod.js
@@ -1,3 +1,17 @@
+import getEnv from 'https://raw.githubusercontent.com/yababay/deno-env-filter/master/mod.js'
+import {Client} from "https://deno.land/x/postgres/mod.ts"
+
+const host = getEnv('PG_HOST') || 'localhost'
+const port = getEnv('PG_PORT') || 5432
+const user = getEnv('PG_USER') || getEnv('PG_USERNAME')
+const password = getEnv('PG_PASSWORD')
+const database = getEnv('PG_DATABASE')
+
+if(!user || !password || !database) throw 'The Postgres connection parameters are unsufficient.'
+
+export default new Client({port, host, user, password, database})
+
+/*
 import * as dotenv from 'https://deno.land/x/denoenv/mod.ts'
 import {Client} from "https://deno.land/x/postgres/mod.ts"
 
@@ -19,4 +33,4 @@ export default new Client(
             return acc
         }, {})
 )
-
+*/
